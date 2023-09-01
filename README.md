17b542ec6b04d33d8df1f5d8e3f0316840db04aa
website-krysta <website.krysta@gmail.com>
2023-09-01 15:12:40 +0530

    inital setup

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # TdhCsrActivities
+
+#install env
+#pip install virtualenv
+# create virtual environment
+#python -m venv env-csr
+
+# activate virtual environment
+#env-csr\Scripts\activate
+
+# install libs
+pip freeze > requirements.txt 
+pip install -r requirements.txt
+
+# create django  project
+Django-admin startproject "tdhcsr"
+
+# create django app 
+python manage.py startapp "csrapp"
+
+# create super user 
+python manage.py createsuperuser
+# migration process 
+python manage.py makemigrations 
+
+# run migrations 
+python manage.py migrate 
+
+# Run project or applications
+python manage.py runserver
--- tdhcsr/tdhcsr/settings.py
+++ tdhcsr/tdhcsr/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'csrapp'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'tdhcsr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +119,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+
+if DEBUG:
+    STATICFILES_DIRS = [STATIC_DIR,]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
