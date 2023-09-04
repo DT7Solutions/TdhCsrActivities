f4fbaa4785b1c192dfcdac1ab86d532a8a8df1fa
website-krysta <website.krysta@gmail.com>
2023-09-04 09:30:21 +0530

    complited inital setup in csr

--- tdhcsr/csrapp/urls.py
+++ tdhcsr/csrapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import Home
+from .views import Home,Csractivities
 urlpatterns = [
     path('', Home, name="home"),
+    path('apply/', Csractivities, name="apply"),
 ]
--- tdhcsr/csrapp/views.py
+++ tdhcsr/csrapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def Home(request):
     return render(request, 'uifiles/home.html')
+
+def Csractivities(request):
+    return render(request, 'uifiles/csrapplication.html')
